e346d35dcf30da5f726dc67cd6b5313b614ac185
carljohansteninge-otto <143940171+carljohansteninge-otto@users.noreply.github.com>
2023-09-18 14:10:28 +0200

    kosdapkodsa

--- data_manipulation_idea.R
+++ data_manipulation_idea.R
@@ -1,1 +1,16 @@
 library(tidyverse)
+install.packages(c("tidyverse", "gapminder", "pacman"))
+pacman::p_load(tidyverse, gapminder)
+data(gapminder)
+head(gapminder)
+
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
+View(gapminder)
+?subset
+
+gapminder.americas<- gapminder %>% filter(continent == "Americas")
+View(gapminder.americas)
+
+lhsadjioas
